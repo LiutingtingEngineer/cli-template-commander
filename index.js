a302ec2d1b9ef6e3d09ce0e5cfda141f0677e813
xuetingliu <xuetingliu@tencent.com>
2022-12-01 23:12:09 +0800

    fix: 删除自定义的help指令

--- index.js
+++ index.js
@@ -3,16 +3,14 @@
 const program = require('commander');
 const init = require('./src/commands/init');
 const version = require('./src/commands/version');
-const help = require('./src/commands/help')
 const add = require('./src/commands/add')
 const deleteTpl = require('./src/commands/delete')
 const list = require('./src/commands/list')
 
-init(program)
 version(program)
-help(program)
 add(program)
 deleteTpl(program)
 list(program)
+init(program)
 
 program.parse(process.argv);
